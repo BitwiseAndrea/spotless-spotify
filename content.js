4ab0dc23cabbc1dce361bbbe30bbe5a7201f80e3
Andrea Fletcher <acfletch53@gmail.com>
2019-11-18 20:51:38 -0800

    Toggle play pause button

--- content.js
+++ content.js
@@ -1,31 +1,13 @@
+var playlistCardPlayButtonSelector = "button._11f5fc88e3dec7bfec55f7f49d581d78-scss";
+
 $(document).ready(function(){
   $("link[rel='icon']").attr("href", chrome.extension.getURL("/images/Spotify_Icon_RGB_White.png"));
 
   $(window).on("load", function(){
      $.ready.then(function(){
-       var playlistCardPlayButtonSelector = "button._11f5fc88e3dec7bfec55f7f49d581d78-scss";
-       var replacePlayButton = function (element) {
-         // Replace play buttons on playlists with play button from nav bar
-         element.classList.remove("_11f5fc88e3dec7bfec55f7f49d581d78-scss")
-         element.classList.add("control-button");
-         element.classList.add("spoticon-play-16");
-         element.classList.add("control-button--circled");
-       };
-
        var playlistButtonElements = $(playlistCardPlayButtonSelector);
        for (var i = 0; i < playlistButtonElements.length; i++) {
-         replacePlayButton(playlistButtonElements[i]);
-       }
-
-       var checkElement = function (element) {
-         if (element.matches && element.matches(playlistCardPlayButtonSelector)) {
-           replacePlayButton(element);
-         } else if (element.querySelector) {
-           var playlistCardPlayButton = element.querySelector(playlistCardPlayButtonSelector);
-           if (playlistCardPlayButton) {
-             replacePlayButton(playlistCardPlayButton);
-           }
-         }
+         setupPlayButton(playlistButtonElements[i]);
        }
 
        // Set up observer
@@ -41,3 +23,33 @@ $(document).ready(function(){
      });
   });
 });
+
+var setupPlayButton = function (element) {
+  // Replace play buttons on playlists with play button from nav bar
+  element.classList.remove("_11f5fc88e3dec7bfec55f7f49d581d78-scss")
+  element.classList.add("control-button");
+  element.classList.add("spoticon-play-16");
+  element.classList.add("control-button--circled");
+
+  // Add click handler for togglePlayPauseButton
+  element.addEventListener("click", function() {
+    if (this.classList.contains("spoticon-play-16")) {
+      this.classList.remove("spoticon-play-16");
+      this.classList.add("spoticon-pause-16");
+    } else if (this.classList.contains("spoticon-pause-16")) {
+      this.classList.remove("spoticon-pause-16");
+      this.classList.add("spoticon-play-16");
+    }
+  }, false);
+};
+
+var checkElement = function (element) {
+  if (element.matches && element.matches(playlistCardPlayButtonSelector)) {
+    setupPlayButton(element);
+  } else if (element.querySelector) {
+    var playlistCardPlayButton = element.querySelector(playlistCardPlayButtonSelector);
+    if (playlistCardPlayButton) {
+      setupPlayButton(playlistCardPlayButton);
+    }
+  }
+}
